93d76eec114bef679b5b95d333f7f68355ef77fb
Diego Paz <diego.paz@cauri.io>
2023-12-07 15:39:12 -0300

    chore: foojay toolchain resolver so JDK 21 auto-provisions when missing

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,7 @@
+plugins {
+    // Auto-provisiona el JDK 21 del toolchain (build.gradle.kts) cuando la máquina que corre el
+    // build no lo tiene instalado; no cambia con qué JDK se corre Gradle mismo.
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
 rootProject.name = "payments-worker"
